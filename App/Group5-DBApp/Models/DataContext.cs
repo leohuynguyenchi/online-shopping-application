d2ab8f2ca11e8bab07cb523e25b54cb8507c0277
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-04-10 21:03:43 -0400

    Added models for CreditCards, Users, and Warehouses

--- App/Group5-DBApp/Models/DataContext.cs
+++ App/Group5-DBApp/Models/DataContext.cs
@@ -9,6 +9,13 @@ namespace Group5_DBApp.Models
             _configuration = configuration;
         }
         public DbSet<Product> Products { get; set; } // DbSet for Product entity
+
+        public DbSet<CreditCards> CreditCards { get; set;}  //Dbset for the CreditCard Entity
+
+        public DbSet<Users> Users { get; set; } // Dbset for the Users entity
+
+        public DbSet<Warehouse> Warehouse  {get; set;} // Dbset for the warehouse entity
+
         public string? DbPath {get;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -25,6 +32,21 @@ namespace Group5_DBApp.Models
                 new Product { prod_id = 3, prod_name = "Nike Elite Championship Basketball", price = 89.99M },
                 new Product { prod_id = 4, prod_name = "Under Armour 495 Indoor/Outdoor Basketball", price = 39.99M },
                 new Product { prod_id = 5, prod_name = "Molten BGG Composite Basketball", price = 44.99M }
+            );
+
+            modelBuilder.Entity<CreditCards>().HasData(
+                new CreditCards { card_id = 1, user_id = 1, card_number = "1234123412341234", expire_date = "2025-12-31"},
+                new CreditCards { card_id = 2, user_id = 2, card_number = "5678567856785678", expire_date = "2025-01-31"}
+            );
+
+            modelBuilder.Entity<Users>().HasData(
+                new Users { user_id = 1, user_type = "Customer", username = "John Doe"},
+                new Users { user_id = 2, user_type = "Staff", username = "Dan Johnson"}
+            );
+
+            modelBuilder.Entity<Warehouse>().HasData(
+                new Warehouse { warehouse_id = 1, address = "123 Main Street", capacity = 100},
+                new Warehouse { warehouse_id = 2, address = "456 Elm Avenue", capacity = 75}
             );
         }
     }
