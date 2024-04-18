165ed5dcdd1477eea7b59b775201e0cfbe0a0c72
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-04-18 15:20:40 -0400

    Added button for deleting order from cart and started backend -  does have bugs

--- App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
+++ App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0ac262410b4f0f68c115720603bf4faaebb9d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5aff957478090866725b7cb8e5d0e3a9db94197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
