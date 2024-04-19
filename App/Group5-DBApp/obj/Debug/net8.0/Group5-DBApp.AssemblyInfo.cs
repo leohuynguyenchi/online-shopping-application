4c0996c830f7c4324ee09a689669efb2590b9b32
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-04-18 21:35:17 -0400

    Implemented fixes to deleting orders from cart

--- App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
+++ App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753f4bd54c570196d14ebb4e2f680ddbc6860005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a42d5671868f10a836f56d1e1c64cfd75742a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
