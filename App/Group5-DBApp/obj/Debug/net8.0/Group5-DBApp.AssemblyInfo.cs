42c42ebd34107da49221447517a1aeb5b891f9b5
Huy Chi Nguyen <nguye2cy@mail.uc.edu>
2024-06-30 15:11:47 -0400

    cleanup Branch

--- App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
+++ App/Group5-DBApp/obj/Debug/net8.0/Group5-DBApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2954b33424a5b3c80617bb1c0477017c907ceb8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f6ea8d69a283868d8cca1ecf458211cc4db78c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group5-DBApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
